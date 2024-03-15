e079202bae718a15408aae9647cd9c0fb60542c0
iqramagray <iqramagray98@gmail.com>
2024-03-15 15:53:28 +0530

    I added these files

---
+++ main.js
@@ -0,0 +1,15 @@
+/*var typed = new Typed(".text",{
+    strings:["Frontend Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});*/
+// var contact = document.getElementsByClassName('contact-form');
+// contact.addEventListener('submit', finish);
+
+// function finish(e){
+//     e.preventdefault();
+//    // window.alert(1)
+//    console.log(e)
+// }
